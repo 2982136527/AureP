e92d4b4ad13b330515628b79ff90a6bd1b00dc9a
2982136527 <2982136527@users.noreply.github.com>
2026-05-18 10:37:58 +0800

    Bump version to 0.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.qiuhu.embyflow"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 5
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
